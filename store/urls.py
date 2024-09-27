a6590c10a95fe7be2fcb7c5b50b39cfbf7b3cc02
Inzamamulhaq01 <156565451+Inzamamulhaq01@users.noreply.github.com>
2024-09-27 10:10:28 +0530

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -6,14 +6,14 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
-    path('add-to-cart/<int:pk>/', views.add_to_cart, name='add_to_cart'),
-    path('cart/', views.cart_view, name='cart_view'),
-    path('order-summary/', views.order_summary, name='order_summary'),
+    path('', login_required(views.product_list), name='product_list'),
+    path('product/<int:pk>/', login_required(views.product_detail), name='product_detail'),
+    path('add-to-cart/<int:pk>/', login_required(views.add_to_cart), name='add_to_cart'),
+    path('cart/', login_required(views.cart_view), name='cart_view'),
+    path('order-summary/', login_required(views.order_summary), name='order_summary'),
     path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
     path('logout/', views.logoutView.as_view(),name='logout'),
-    path('orders/', views.order_list, name='order_list'), 
+    path('orders/', login_required(views.order_list), name='order_list'), 
     path('register/',views.registerView.as_view(),name='register'),
     
     
