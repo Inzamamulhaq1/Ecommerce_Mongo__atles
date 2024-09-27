bc101742b7d50b557acbb8733cd1d877f5c63c63
Inzamamulhaq01 <156565451+Inzamamulhaq01@users.noreply.github.com>
2024-09-27 10:16:32 +0530

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
